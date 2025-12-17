50150cba3e210b5cc0d048eb83481b50ec94c374
bala-amypo <bala@amypo.in>
2025-12-17 09:59:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,40 +1,58 @@
-package com.example.sql.Entitydata;
+package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import jakarta.persistence.Id;
 
-@Entity
-public class Student {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+@Entity           //automates the table creation
+ //`@Table()
+public class Student{                             //in entity -> class name=table name
+
+    @Id           //primary key annotation
+    private int id;   
+    // @GeneratedValue(strategy=GenerationType.IDENTITY)      //auto increment    -> if this given means remove 'this.id=id' in parameterized constructor, if not given means keep it
+    private String email;       //columns of table ->id,name,email,cgpa
     private String name;
-    private String email;
+    private String cgpa;
+    
     public int getId() {
         return id;
     }
-    public void setId(Integer id) {
+
+    public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
-    public Student(Integer id, String name, String email) {
-        this.id = id;
+
+    public String getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(String cgpa) {
+        this.cgpa = cgpa;
+    }
+    public Student(int id, String name, String email,String cgpa ) { //parameterized constructor for setting values
+        this.id = id;            
         this.name = name;
         this.email = email;
+        this.cgpa = cgpa;
     }
-    public Student(){
-}
+
+    public Student() {    //default constructor for getting values
+    
+    }
 }
